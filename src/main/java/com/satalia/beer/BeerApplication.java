c449a3fa4e10587a7b83663faf2902bcd5550c0d
Deividas Balandis <deividas.balandis@gmail.com>
2022-09-20 21:18:25 +0300

    Removed csv files. Finished data import to H2 database.

--- src/main/java/com/satalia/beer/BeerApplication.java
+++ src/main/java/com/satalia/beer/BeerApplication.java
@@ -2,7 +2,6 @@ package com.satalia.beer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BeerApplication {
--- src/main/java/com/satalia/beer/entity/CategoryEntity.java
+++ src/main/java/com/satalia/beer/entity/CategoryEntity.java
@@ -1,5 +1,22 @@
 package com.satalia.beer.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "CATEGORY")
 public class CategoryEntity {
+
+    @Id
+    private Long id;
+    private String cat_name;
+    private Date lastModified;
 
 }
